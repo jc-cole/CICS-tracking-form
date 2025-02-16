a2178e71734e73b12da893bd602fb783bdc786bd
jc-cole <jaredcole@umass.edu>
2025-02-15 21:02:33 -0500

    make CourseCard class

---
+++ dom-elements/CourseCard.js
@@ -0,0 +1,9 @@
+export class CourseCard {
+    constructor(courseNode) {
+        this.courseNode = courseNode
+        // holds the dom element representing the courseNode
+        this.element = document.createElement("div")
+    }
+
+    // to implement: makeDomElement()
+}
